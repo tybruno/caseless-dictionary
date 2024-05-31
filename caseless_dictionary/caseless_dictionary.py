3774bf38a6703556634f44acb1c42abb60010618
tybruno <tybruno@cisco.com>
2024-05-31 16:07:27 -0600

    adding more cases.

--- caseless_dictionary/caseless_dictionary.py
+++ caseless_dictionary/caseless_dictionary.py
@@ -1,95 +1,42 @@
-""" Caseless Dictionary and related objects.
+"""
+Caseless Dictionary and related objects.
 
 Objects provided by this module:
    `CaselessDict` - Keys are case-folded case.
    `TitleCaselessDict` - Keys are in title case.
    `UpperCaselessDict` - Keys are in upper case.
-
+   `SnakeCaselessDict` - Keys are in snake case.
+   `KebabCaselessDict` - Keys are in kebab case.
+   `ConstantCaselessDict` - Keys are in constant case.
 """
-import typing
+from typing import Hashable
+from modifiable_items_dictionary import ModifiableItemsDict
 
-import modifiable_items_dictionary
-
-
-def _case_fold(value: typing.Any):
-    """strip then casefold a *str*
-
-    Example:
-        >>> _case_fold("   CamelCase   ")
-        'camelcase'
-        >>> _case_fold(2) # Not a *str*
-        2
-
-    Args:
-        value: If an instance of a string strip then casefold the *v*
-
-    Returns:
-        The stripped and casefolded *str*. If not an instance of *str* return the v unchanged.
-
-    """
-    if isinstance(value, str):
-        _stripped_value: str = value.strip()
-        _value_case_folded: str = _stripped_value.casefold()
-        return _value_case_folded
-    return value
+from caseless_dictionary.cases import (
+    case_fold,
+    upper,
+    lower,
+    title,
+    snake_case,
+    kebab_case,
+    constant_case,
+)
 
 
-def _upper(value: typing.Any):
-    """strip the string then convert to uppercase.
-
-    Example:
-        >>> _upper("   CamelCase   ")
-        'CAMELCASE'
-        >>> _upper(["not of type *str*"]) # Not a *str*
-        ['not of type *str*']
-
-    Args:
-        value: If an instance of a string strip then convert the *v* to an uppercase *str*
-
-    Returns:
-        The stripped and uppercased *str*. If not an instance of *str* return the v unchanged.
-    """
-    if isinstance(value, str):
-        _stripped_value: str = value.strip()
-        _value_upper: str = _stripped_value.upper()
-        return _value_upper
-    return value
-
-
-def _title(value: typing.Any):
-    """strip the string then convert to title.
-
-    Example:
-        >>> _title("   lower UPPER CamelCase   ")
-        'Lower Upper Camelcase'
-        >>> _title(["not of type *str*"]) # Not a *str*
-        ['not of type *str*']
-
-    Args:
-        value: If an instance of a string strip then convert the *v* to an uppercase *str*
-
-    Returns:
-        The stripped and uppercased *str*. If not an instance of *str* return the v unchanged.
-    """
-    if isinstance(value, str):
-        _stripped_value: str = value.strip()
-        _value_title: str = _stripped_value.title()
-        return _value_title
-    return value
-
-
-class CaselessDict(modifiable_items_dictionary.ModifiableItemsDict):
-    """Case-insensitive Dictionary class where the keys thar are strings are casefolded.
+class CaselessDict(ModifiableItemsDict):
+    """Case-insensitive Dictionary class where the keys thar are strings
+    are casefolded.
 
     CaselessDict() -> new empty caseless dictionary
-    CaselessDict(mapping) -> new caseless dictionary initialized from a mapping object's
-        (__key, v) pairs
+    CaselessDict(mapping) -> new caseless dictionary initialized from a
+        mapping object's (__key, v) pairs
     CaselessDict(__m) -> new caseless dictionary initialized as if via:
         d = CaselessDict()
         for k, v in __m:
             d[k] = v
-    CaselessDict(**kwargs) -> new caseless dictionary initialized with the name=v pairs
-        in the keyword argument list.  For example:  CaselessDict(one=1, two=2)
+    CaselessDict(**kwargs) -> new caseless dictionary initialized with
+        the name=v pairs in the keyword argument list.
+        For example:  CaselessDict(one=1, two=2)
 
     Example:
         >>> normal_dict: dict = {"  lower   ": 1, "UPPER ": 2, "CamelCase": 3}
@@ -100,9 +47,10 @@ class CaselessDict(modifiable_items_dictionary.ModifiableItemsDict):
         2
     """
 
-    _key_modifiers = staticmethod(_case_fold)
+    __slots__ = ()
+    _key_modifiers = [case_fold]
 
-    def __missing__(self, key: typing.Hashable) -> None:
+    def __missing__(self, key: Hashable) -> None:
         """Handle missing __key.
         Args:
             key: The Hashable __key that is missing.
@@ -110,25 +58,85 @@ class CaselessDict(modifiable_items_dictionary.ModifiableItemsDict):
         Raises:
             *KeyError* with a more descriptive error for caseless keys.
         """
-        error: KeyError = KeyError(
-            "Missing key of some case variant of ", key
-        )
+        error: KeyError = KeyError('Missing key of some case variant of ', key)
 
         raise error
 
 
+class CaseFoldCaselessDict(CaselessDict):
+    """
+    Case-insensitive Dictionary class where the keys that are strings are
+    case-folded.
+
+    CaseFoldCaselessDict() -> new empty case-folded caseless dictionary
+    CaseFoldCaselessDict(mapping) -> new case-folded caseless dictionary
+        initialized from a mapping object's (key, value) pairs
+    CaseFoldCaselessDict(iterable) -> new case-folded caseless dictionary
+        initialized as if via:
+        d = CaseFoldCaselessDict()
+        for k, v in iterable:
+            d[k] = v
+    CaseFoldCaselessDict(**kwargs) -> new case-folded caseless dictionary
+        initialized with the name=value pairs in the keyword argument list.
+        For example:  CaseFoldCaselessDict(one=1, two=2)
+
+    Example:
+    >>> normal_dict: dict = {"  lower   ": 1, "UPPER ": 2, "CamelCase": 3}
+    >>> case_fold_caseless_dict = CaseFoldCaselessDict(normal_dict)
+    >>> case_fold_caseless_dict
+    {'lower': 1, 'upper': 2, 'camelcase': 3}
+    >>> case_fold_caseless_dict[" UPpeR  "]
+    2
+    """
+
+    __slots__ = ()
+
+
+class LowerCaselessDict(CaselessDict):
+    """
+    Case-insensitive Dictionary class where the keys that are strings are
+    in lower case.
+
+    LowerCaselessDict() -> new empty lower caseless dictionary
+    LowerCaselessDict(mapping) -> new lower caseless dictionary
+        initialized from a mapping object's (key, value) pairs
+    LowerCaselessDict(iterable) -> new lower caseless dictionary
+        initialized as if via:
+        d = LowerCaselessDict()
+        for k, v in iterable:
+            d[k] = v
+    LowerCaselessDict(**kwargs) -> new lower caseless dictionary
+        initialized with the name=value pairs in the keyword argument list.
+        For example:  LowerCaselessDict(one=1, two=2)
+
+    Example:
+    >>> normal_dict: dict = {"  lower   ": 1, "UPPER ": 2, "CamelCase": 3}
+    >>> lower_caseless_dict = LowerCaselessDict(normal_dict)
+    >>> lower_caseless_dict
+    {'lower': 1, 'upper': 2, 'camelcase': 3}
+    >>> lower_caseless_dict[" UPpeR  "]
+    2
+    """
+
+    __slots__ = ()
+    _key_modifiers = [lower]
+
+
 class UpperCaselessDict(CaselessDict):
-    """Case-insensitive Dictionary class where the keys that are strings are in upper case.
+    """Case-insensitive Dictionary class where the keys that are strings are
+    in upper case.
 
     UpperCaselessDict() -> new empty upper caseless dictionary
-    UpperCaselessDict(mapping) -> new upper caseless dictionary initialized from a mapping object's
-        (__key, v) pairs
-    UpperCaselessDict(__m) -> new upper caseless dictionary initialized as if via:
+    UpperCaselessDict(mapping) -> new upper caseless dictionary initialized
+        from a mapping object's (__key, v) pairs
+    UpperCaselessDict(__m) -> new upper caseless dictionary initialized as if
+        via:
         d = UpperCaselessDict()
         for k, v in __m:
             d[k] = v
-    UpperCaselessDict(**kwargs) -> new upper caseless dictionary initialized with the name=v pairs
-        in the keyword argument list.  For example:  UpperCaselessDict(one=1, two=2)
+    UpperCaselessDict(**kwargs) -> new upper caseless dictionary initialized
+        with the name=v pairs in the keyword argument list.
+        For example:  UpperCaselessDict(one=1, two=2)
 
     Example:
     >>> normal_dict: dict = {"  lower   ": 1, "UPPER ": 2, "CamelCase": 3}
@@ -139,21 +147,25 @@ class UpperCaselessDict(CaselessDict):
     True
     """
 
-    _key_modifiers = staticmethod(_upper)
+    __slots__ = ()
+    _key_modifiers = [upper]
 
 
 class TitleCaselessDict(CaselessDict):
-    """Case-insensitive Dictionary class where the keys that are strings are in Title Case.
+    """Case-insensitive Dictionary class where the keys that are strings are
+    in Title Case.
 
     TitleCaselessDict() -> new empty title caseless dictionary
-    TitleCaselessDict(mapping) -> new title caseless dictionary initialized from a mapping object's
-        (__key, v) pairs
-    TitleCaselessDict(__m) -> new title caseless dictionary initialized as if via:
+    TitleCaselessDict(mapping) -> new title caseless dictionary initialized
+        from a mapping object's (__key, v) pairs.
+    TitleCaselessDict(__m) -> new title caseless dictionary initialized as if
+        via:
         d = TitleCaselessDict()
         for k, v in __m:
             d[k] = v
-    TitleCaselessDict(**kwargs) -> new title caseless dictionary initialized with the name=v pairs
-        in the keyword argument list.  For example:  TitleCaselessDict(one=1, two=2)
+    TitleCaselessDict(**kwargs) -> new title caseless dictionary initialized
+        with the name=v pairs in the keyword argument list.
+        For example:  TitleCaselessDict(one=1, two=2)
 
     Example:
     >>> normal_dict: dict = {"  lower   ": 1, "UPPER ": 2, "CamelCase": 3}
@@ -164,4 +176,94 @@ class TitleCaselessDict(CaselessDict):
     True
     """
 
-    _key_modifiers = staticmethod(_title)
+    __slots__ = ()
+    _key_modifiers = [title]
+
+
+class SnakeCaselessDict(CaselessDict):
+    """Case-insensitive Dictionary class where the keys that are strings are
+    in Snake Case.
+
+    SnakeCaselessDict() -> new empty snake caseless dictionary
+    SnakeCaselessDict(mapping) -> new snake caseless dictionary initialized
+        from a mapping object's (__key, v) pairs
+    SnakeCaselessDict(__m) -> new snake caseless dictionary initialized as if
+        via:
+        d = SnakeCaselessDict()
+        for k, v in __m:
+            d[k] = v
+    SnakeCaselessDict(**kwargs) -> new snake caseless dictionary initialized
+        with the name=v pairs in the keyword argument list.
+        For example:  SnakeCaselessDict(one=1, two=2)
+
+    Example:
+    >>> from caseless_dictionary import SnakeCaselessDict
+    >>> normal_dict: dict = {"  lower   ": 1, "UPPER ": 2, "CamelCase": 3}
+    >>> snake_caseless_dict = SnakeCaselessDict(lower=1, UPPER=2, CamelCase=3)
+    >>> snake_caseless_dict
+    {'lower': 1, 'upper': 2, 'camel_case': 3}
+    >>> "  CamelCase  " in snake_caseless_dict
+    True
+    """
+
+    __slots__ = ()
+    _key_modifiers = [snake_case]
+
+
+class KebabCaselessDict(CaselessDict):
+    """Case-insensitive Dictionary class where the keys that are strings are
+    in Kebab Case.
+
+    KebabCaselessDict() -> new empty kebab caseless dictionary
+    KebabCaselessDict(mapping) -> new kebab caseless dictionary initialized
+        from a mapping object's (__key, v) pairs
+    KebabCaselessDict(__m) -> new kebab caseless dictionary initialized as if
+        via:
+        d = KebabCaselessDict()
+        for k, v in __m:
+            d[k] = v
+    KebabCaselessDict(**kwargs) -> new kebab caseless dictionary initialized
+        with the name=v pairs in the keyword argument list.
+        For example:  KebabCaselessDict(one=1, two=2)
+
+    Example:
+    >>> normal_dict: dict = {"  lower   ": 1, "UPPER ": 2, "CamelCase": 3}
+    >>> kebab_caseless_dict = KebabCaselessDict(lower=1, UPPER=2, CamelCase=3)
+    >>> kebab_caseless_dict
+    {'lower': 1, 'upper': 2, 'camel-case': 3}
+    >>> "  CamelCase  " in kebab_caseless_dict
+    True
+    """
+
+    __slots__ = ()
+    _key_modifiers = [kebab_case]
+
+
+class ConstantCaselessDict(CaselessDict):
+    """Case-insensitive Dictionary class where the keys that are strings are
+     in Constant Case.
+
+    ConstantCaselessDict() -> new empty constant caseless dictionary
+    ConstantCaselessDict(mapping) -> new constant caseless dictionary
+        initialized from a mapping object's (__key, v) pairs
+    ConstantCaselessDict(__m) -> new constant caseless dictionary initialized
+        as if via:
+        d = ConstantCaselessDict()
+        for k, v in __m:
+            d[k] = v
+    ConstantCaselessDict(**kwargs) -> new constant caseless dictionary
+        initialized with the name=v pairs in the keyword argument list.
+        For example:  ConstantCaselessDict(one=1, two=2)
+
+    Example:
+    >>> normal_dict: dict = {"  lower   ": 1, "UPPER ": 2, "CamelCase": 3}
+    >>> constant_caseless_dict = ConstantCaselessDict(lower=1, UPPER=2,
+    ... CamelCase=3)
+    >>> constant_caseless_dict
+    {'LOWER': 1, 'UPPER': 2, 'CAMEL_CASE': 3}
+    >>> "  CamelCase  " in constant_caseless_dict
+    True
+    """
+
+    __slots__ = ()
+    _key_modifiers = [constant_case]
--- tests/test_change_case.py
+++ tests/test_change_case.py
@@ -1,20 +1,41 @@
-import typing
+"""Test cases for the change_case module.
 
+This module contains test cases for the change_case module. It tests the
+functions that change the case of a string.
+
+Classes:
+    TestCaseFold: Test case for the case_fold function.
+    TestTitle: Test case for the title function.
+    TestUpper: Test case for the upper function.
+    TestLowerCase: Test case for the lower function.
+    TestSnakeCase: Test case for the snake_case function.
+    TestKebabCase: Test case for the kebab_case function.
+"""
+import typing
+import re
 import pytest
 
-from caseless_dictionary.caseless_dictionary import _case_fold, _upper, _title
+from caseless_dictionary.cases import (
+    case_fold,
+    upper,
+    title,
+    snake_case,
+    kebab_case,
+    lower,
+    constant_case,
+)
 
 
 @pytest.fixture(
     params=(
-            1,
-            5.56,
-            True,
-            "   Tittle  ",
-            "lower  ",
-            "   UPPER",
-            "CamelCase  ",
-            ["NotTouched"],
+        1,
+        5.56,
+        True,
+        '   Tittle  ',
+        'lower  ',
+        '   UPPER',
+        'CamelCase  ',
+        ['NotTouched'],
     )
 )
 def data(request) -> typing.Any:
@@ -28,7 +49,7 @@ class TestCaseFold:
         if isinstance(data, str):
             expected = data.strip().casefold()
 
-        actual = _case_fold(data)
+        actual = case_fold(data)
         assert actual == expected
 
 
@@ -38,7 +59,7 @@ class TestTitle:
         if isinstance(data, str):
             expected = data.strip().title()
 
-        actual = _title(data)
+        actual = title(data)
         assert actual == expected
 
 
@@ -48,5 +69,45 @@ class TestUpper:
         if isinstance(data, str):
             expected = data.strip().upper()
 
-        actual = _upper(data)
+        actual = upper(data)
+        assert actual == expected
+
+
+class TestSnakeCase:
+    def test_snake_case(self, data):
+        expected = data
+        if isinstance(data, str):
+            expected = data.strip().replace(' ', '_').casefold()
+
+        actual = snake_case(data)
+        assert actual == expected
+
+
+class TestKebabCase:
+    def test_kebab_case(self, data):
+        expected = data
+        if isinstance(data, str):
+            expected = data.strip().replace(' ', '-').casefold()
+
+        actual = kebab_case(data)
+        assert actual == expected
+
+
+class TestConstantCase:
+    def test_constant_case(self, data):
+        expected = data
+        if isinstance(data, str):
+            expected = data.strip().replace(' ', '_').upper()
+
+        actual = constant_case(data)
+        assert actual == expected
+
+
+class TestLowerCase:
+    def test_lower_case(self, data):
+        expected = data
+        if isinstance(data, str):
+            expected = data.strip().lower()
+
+        actual = lower(data)
         assert actual == expected
